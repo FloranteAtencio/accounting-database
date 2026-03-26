07835a4fbf29c92dd5db04e2bd150f64dd348e45
Florante Atencio <144699572+FloranteAtencio@users.noreply.github.com>
2026-03-26 16:39:42 +0800

    Update 08_paritioning.sql

--- schema/08_paritioning.sql
+++ schema/08_paritioning.sql
@@ -1,17 +1,16 @@
-CREATE OR REPLACE FUNCTION Finance.partion_weekly_basis (tableselected text, schemaselected text)
+CREATE OR REPLACE FUNCTION Finance.partion_weekly_basis(tableselected text, schemaselected text)
 RETURNS void AS $$
 DECLARE
     s_date date := date_trunc('week', current_date);
     e_date date := s_date + interval '7 days';
     part_name text;    
 BEGIN
-
     part_name := schemaselected || '.' || tableselected || '_' || to_char(s_date, 'YYYY_MM') || 'wk' || extract(week from s_date);
 
     EXECUTE format(
         'CREATE TABLE %I PARTITION OF %I.%I 
         FOR VALUES FROM (%L) TO (%L);',
-        part_name,schemaselected,tableselected,s_date,e_date
+        part_name, schemaselected, tableselected, s_date, e_date
     );
 
     EXCEPTION
@@ -20,24 +19,21 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION Finance.partion_monthly_basis (tableselected text, schemaselected text)
+CREATE OR REPLACE FUNCTION Finance.partion_weekly_monthly(tableselected text, schemaselected text)
 RETURNS void AS $$
 DECLARE
-
     s_date date := date_trunc('month', current_date);
     e_date date := s_date + interval '30 days';
-    part_name text;
-    
+    part_name text;    
 BEGIN
-    BEGIN
-    part_name := schemaselected || '.' || tableselected || '_' || to_char(s_date, 'YYYY_MM') || 'wk' || extract(week from s_date);
+    part_name := schemaselected || '.' || tableselected || '_' || to_char(s_date, 'YYYY_MM') || 'wk' || extract(month from s_date);
 
     EXECUTE format(
         'CREATE TABLE %I PARTITION OF %I.%I 
         FOR VALUES FROM (%L) TO (%L);',
-        part_name,schemaselected,tableselected,s_date,e_date
+        part_name, schemaselected, tableselected, s_date, e_date
     );
-    
+
     EXCEPTION
         WHEN OTHERS THEN
             RAISE EXCEPTION 'Transaction failed: %', SQLERRM;
