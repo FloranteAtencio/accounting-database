a920d47bdb2ba71aef4a4ba45802bf5c0ef5cd93
Florante Atencio <144699572+FloranteAtencio@users.noreply.github.com>
2026-03-27 21:04:16 +0800

    Some types changes

--- staging/V2_staging_tablespace.sh
+++ staging/V2_staging_tablespace.sh
@@ -9,20 +9,19 @@ CONTAINER_NAME="stagin_env"
 LOG_FILE="/var/log/erp-V2_staging.log"
 
 mkdir -p "$SCRIPT_DIR"
-touch -p "$LOG_FILE"
 
-echo "[$(date)] V2_staginb_tablespace testing" >> "$LOG_FILE"
+echo "[$(date)] V2_staging_tablespace testing" >> "$LOG_FILE"
 
-docker exec -i $CONTAINER_NAME psql -U $DB_USER -d $DB_NAME < $SCRIPT_DIR/01_Startup_staging.sql
-if [ $? -eq 0]; then
-    echo "[$(date)] Staging Startup succesful" >> "$LOG_FILE"
+docker exec -i $CONTAINER_NAME psql -U $DB_USER -d $DB_NAME < "$SCRIPT_DIR/01_Startup_staging.sql"
+if [ $? -eq 0 ]; then
+    echo "[$(date)] Staging Startup SQL alright!" >> "$LOG_FILE"
 else
-    echo "[$(date)] Staging Startup Operation failed" >> "$LOG_FILE"
+    echo "[$(date)] Staging Startup SQL failed!" >> "$LOG_FILE"
     exit 1
 fi
 
-docker exec -i $CONTAINER_NAME psql -U $DB_USER -d $DB_NAME < $SCRIPT_DIR/02_tables_staging.sql
-if [ $? -eq 0]; then
+docker exec -i $CONTAINER_NAME psql -U $DB_USER -d $DB_NAME < "$SCRIPT_DIR/02_tables_staging.sql"
+if [ $? -eq 0 ]; then
     echo "[$(date)] Staging Startup succesful" >> "$LOG_FILE"
 else
     echo "[$(date)] Staging Startup Operation failed" >> "$LOG_FILE"
