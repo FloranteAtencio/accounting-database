eae37798fed1658d0ad0a355dd756ff0c0c50695
Florante Atencio <144699572+FloranteAtencio@users.noreply.github.com>
2026-03-27 20:37:24 +0800

    from sql to sh of v2 staging_tablespace

---
+++ staging/V2_staging_tablespace.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#./staing/V2_staginb_tablespace.sh
+
+
+SCRIPT_DIR = "../tmp"
+DB_NAME = "erp_staging"
+DB_USER = "staging_user"
+CONTAINER_NAME = "stagin_env"
+LOG_FILE = "/var/log/erp-V2_staging.log
+
+echo "[$(date)] V2_staginb_tablespace testing" >> "$LOG_FILE"
+
+docker exec -i $CONTAINER_NAME psql -U $DB_USER -d $DB_NAME < $SCRIPT_DIR/01_Startup_staging.sql
+if [ $? -eq 0]; then
+    echo "[$(date)] Staging Startup succesful" >> "$LOG_FILE"
+else
+    echo "[$(date)] Staging Startup Operation failed" >> "$LOG_FILE"
+    exit 1
+fi
+
+docker exec -i $CONTAINER_NAME psql -U $DB_USER -d $DB_NAME < $SCRIPT_DIR/02_tables_staging.sql
+if [ $? -eq 0]; then
+    echo "[$(date)] Staging Startup succesful" >> "$LOG_FILE"
+else
+    echo "[$(date)] Staging Startup Operation failed" >> "$LOG_FILE"
+    exit 1
+fi
--- staging/V2_staging_tablespace.sql
+++
@@ -1,2 +0,0 @@
-docker exec -i staging_env psql -U staging_user -d erp_staging < ../tmp/01_Startup_staging.sql
-docker exec -i staging_env psql -U staging_user -d erp_staing  < ../tmp/02_tables_staging.sql
