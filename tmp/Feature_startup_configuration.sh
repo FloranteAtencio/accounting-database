fcd34fce919e75b06deaef047d8fd28b6ac0eeb0
Florante Atencio <144699572+FloranteAtencio@users.noreply.github.com>
2026-04-25 21:01:40 +0800

    bookkeeping

--- tmp/Feature_startup_configuration.sh
+++ tmp/Feature_startup_configuration.sh
@@ -117,21 +117,22 @@ else
    echo "[$(date)] Sample Data Failed" >> "$LOG_FILE"
 fi
 
+
+docker exec -i "$CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" < "$SCRIPT_DIR/data_sample.sql"
+if [ $? -eq 0 ]; then
+   echo "[$(date)] Sample Data Complete" >> "$LOG_FILE"
+else
+   echo "[$(date)] Sample Data Failed" >> "$LOG_FILE"
+fi
+
+
 # Run Tables SQL
-docker exec -i "$CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" < "$SCRIPT_DIR_SCHEMA/08_Constraint.sql"
+docker exec -i "$CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" < "$SCRIPT_DIR_SCHEMA/data_testing.sql"
 if [ $? -eq 0 ]; then
     echo "[$(date)] Feature Constraint SQL alright!" >> "$LOG_FILE"
 else
     echo "[$(date)] Feature Constraint SQL failed!" >> "$LOG_FILE"
     exit 1
-fi
-
-
-docker exec -i "$CONTAINER_NAME" psql -U "$DB_USER" -d "$DB_NAME" < "$SCRIPT_DIR/sample_data.sql"
-if [ $? -eq 0 ]; then
-   echo "[$(date)] Sample Data Complete" >> "$LOG_FILE"
-else
-   echo "[$(date)] Sample Data Failed" >> "$LOG_FILE"
 fi
 
 echo "[$(date)] Successful Feature" >> "$LOG_FILE"
