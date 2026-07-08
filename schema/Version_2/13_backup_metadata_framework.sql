99a41aafec4ac46620fcd1ce9353e713e9faa984
Florante Atencio <144699572+FloranteAtencio@users.noreply.github.com>
2026-07-09 02:41:21 +0800

    back meta data

--- schema/Version_2/13_backup_metadata_framework.sql
+++ schema/Version_2/13_backup_metadata_framework.sql
@@ -153,16 +153,16 @@ BEGIN
         t.database_name,
         MAX(b.backup_date),
         EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - MAX(b.backup_date)))::INT / 3600,
-        b.status,
+        MAX(b.status),
         t.rto_minutes,
         t.rpo_minutes,
         (EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - MAX(b.backup_date)))::INT / 60) <= t.rpo_minutes,
         MAX(b.restored_at),
-        CASE WHEN b.status = 'SUCCESS' AND b.restored_at > CURRENT_TIMESTAMP - INTERVAL '7 days' 
+        CASE WHEN MAX(b.status) = 'SUCCESS' AND MAX(b.restored_at) > CURRENT_TIMESTAMP - INTERVAL '7 days' 
              THEN TRUE ELSE FALSE END
     FROM dba_admin.rpo_rto_targets t
     LEFT JOIN dba_admin.backup_history b ON t.database_name = b.database_name
-    GROUP BY t.database_name, t.rto_minutes, t.rpo_minutes, b.status;
+    GROUP BY t.database_name, t.rto_minutes, t.rpo_minutes;
 END;
 $$ LANGUAGE plpgsql;
 
