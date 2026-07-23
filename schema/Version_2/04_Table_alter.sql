aea1aec9668baa725970b8c30063301556908047
Florante Atencio <144699572+FloranteAtencio@users.noreply.github.com>
2026-07-23 19:55:57 +0800

    tabler alter sql finance.product to products

--- schema/Version_2/04_Table_alter.sql
+++ schema/Version_2/04_Table_alter.sql
@@ -15,7 +15,7 @@ ALTER TABLE Finance.customers ADD COLUMN client_id INT REFERENCES Finance.client
 
 ALTER TABLE Finance.inventory_audits ADD COLUMN client_id INT REFERENCES Finance.clients (client_id);
 
-ALTER TABLE Finance.product ADD COLUMN client_id INT REFERENCES Finance.clients (client_id);
+ALTER TABLE Finance.products ADD COLUMN client_id INT REFERENCES Finance.clients (client_id);
 
 ALTER TABLE Finance.clients 
     ADD inventory_method VARCHAR(20) CHECK ( inventory_method IN ('PERPETUAL','PERIODIC')),
